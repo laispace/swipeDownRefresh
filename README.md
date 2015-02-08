8aa4e9764da54c97871b0f953edde9d09d0c0253
laispace <qingsheng.lai@gmail.com>
2015-02-08 13:16:29 +0800

    add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# 一个简单的下拉刷新组件
+
+iOS 原生自带下拉阻力, 但阻力太大
+而 Android 没有自带下拉效果, 两个平台很不一致
+
+所以这个 swipe-down-refresh.js 做的是屏蔽 iOS 的默认下拉效果, 使用自模拟的下拉阻力, 让下拉刷新在 iOS 和 Android 中体验一致.
+
+
+# 体验地址
+
+手机端打开, [体验地址](laispace.github.io/swipeDownRefresh)
---
+++ lai-swipe-down-refresh.js
@@ -0,0 +1,143 @@
+function Swipe (options) {
+    this._init(options);
+
+    this.bindEvents();
+}
+Swipe.prototype = {
+    swipeTipsEl: '<div class="swipe-tips"></div>',
+    _init: function (options) {
+        this.options = options || {};
+        this.swipeDownDistance = this.options.swipeDownDistance ? this.options.swipeDownDistance : 300;
+        this.swipeDownTipsDistance = this.options.swipeDownTipsDistance ? this.options.swipeDownTipsDistance : (this.swipeDownDistance/2);
+        this.$el = this.options.el ? $(this.options.el) : $('body');
+        this.$scrollEl = $.os.ios ? $(this.$el) : $(window);
+
+        this.isShow = false;
+        this.touch = {
+            y1: 0,
+            y2: 0,
+            dy: 0
+        };
+
+        this.state = -1;
+
+        this.$parentEl = this.$el.parent();
+        this.$swipeTipsEl = $(this.swipeTipsEl);
+        this.$parentEl.css('position', 'relative');
+        this.$parentEl.prepend(this.$swipeTipsEl);
+    },
+    getScrollTop: function () {
+        return this.$scrollEl.scrollTop();
+    },
+    setScrollTop: function (top) {
+        this.$scrollEl.scrollTop(top);
+    },
+    getAtanDistance: function (distance) {
+        var self = this;
+        return Math.atan(distance / self.swipeDownDistance) * self.swipeDownDistance;
+    },
+    swipeDown: function (distance) {
+        var self = this;
+        self.$el.css({
+            'transform': 'translate3d(0, ' + distance + 'px, 0)',
+            '-webkit-transform': 'translate3d(0, ' + distance + 'px, 0)'
+        });
+
+        // tougle swipe tips
+        if (distance >= 0) {
+            self.state = -1;
+        }
+        if (distance >= self.swipeDownTipsDistance) {
+            self.state = 0;
+        }
+        if (distance >= self.swipeDownDistance) {
+            self.state = 1;
+        }
+        self.showTips();
+    },
+    swipeUp: function (distance) {
+        var self = this;
+        self.$el.animate({
+            'transform': 'translate3d(0, ' + distance + 'px, 0)',
+            '-webkit-transform': 'translate3d(0, ' + distance + 'px, 0)'
+        }, 1000, 'ease');
+
+        if (distance <= self.swipeDownDistance) {
+            self.state = 2;
+        }
+        if (distance === 0){
+            self.state = -1;
+        }
+        self.showTips();
+    },
+    showTips: function () {
+        var self = this;
+        var state = self.state;
+        if (state > -1) {
+            var tips = self.options.swipeDownTips[state];
+            self.$swipeTipsEl.text(tips).show();
+        } else {
+            self.$swipeTipsEl.text('').hide();
+        }
+    },
+    bindEvents: function () {
+        var self = this;
+        var $el = this.$el;
+        $el.on('touchstart', function (e) {
+            self.touch.y1 = e.touches[0].pageY;
+            self.touch.dy = 0;
+
+            if (self.getScrollTop() === 0) {
+                $el.on('touchmove', moveHandle);
+            }
+        });
+        function moveHandle (e) {
+            self.touch.y2 = e.touches[0].pageY;
+            self.touch.dy = self.touch.y2 - self.touch.y1;
+
+            if (self.touch.dy > 0) {
+                e.preventDefault();
+            }
+
+            // swipe down with finger
+            if (self.touch.dy > 0) {
+                // swiping down
+                var distance = self.isShow ? (self.swipeDownDistance + self.touch.dy) : self.touch.dy;
+
+                // recalculate the distance
+                distance = self.getAtanDistance(distance);
+
+                self.swipeDown(distance);
+            }
+
+
+        }
+        $el.on('touchend', function () {
+            $el.off('touchmove', moveHandle);
+
+            self.isShow = (self.touch.dy >= self.swipeDownDistance);
+
+            var distance = self.isShow ? self.swipeDownDistance : 0;
+
+            // recalculate the distance
+            distance = self.getAtanDistance(distance);
+
+            self.swipeUp(distance);
+
+            if (self.isShow) {
+                setTimeout(function () {
+                    self.state = 3;
+                    self.showTips();
+                    // test
+                    laiUtil.appendTestDiv(2);
+                }, 3000);
+                setTimeout(function () {
+                    self.state = -1;
+                    self.swipeUp(0);
+                    self.isShow = false;
+                }, 4000);
+            }
+
+        });
+    }
+};
